d2b0b6376317bd3df09c7a9e9dd9102f901cd2c5
PeculiarE <perhisohwode@gmail.com>
2021-04-14 14:39:20 +0100

    removed views and public folders pec
    removed the folders that contain views and public files. also, added dist to .gitignore and removed it from being tracked by git. also, installed and connected postgres. also, created a table and inserted a sample user

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import createError from 'http-errors';
 import express, { json, urlencoded } from 'express';
-import { join } from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
@@ -9,15 +8,10 @@ import usersRouter from '#routes/users';
 
 const app = express();
 
-// view engine setup
-app.set('views', join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.status(200).json({
+    message: 'Welcome to our new project',
+  });
 });
 
 export default router;
